0989ef83f6481f5121243bea60a6ac052a57e21e
Argha Banerjee <gabbu@arghas-mbp.home>
2017-03-12 00:50:50 +0000

    Firt commit

---
+++ src/main/java/com/example/RestExample.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class RestExample {
+	
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String testMethod(){
+		return "Hello";
+	}
+
+}
